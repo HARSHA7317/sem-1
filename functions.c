a945cfc89b164584c3c5de39a4c42a7538183892
HARSHA7317 <n.harshavardhan002@gmail.com>
2025-12-03 13:18:06 +0530

    Add files via upload

---
+++ functions.c
@@ -0,0 +1,19 @@
+ #include <stdio.h>
+ 
+ int sum(int ,int );
+ int main(){
+    int a,b,s;
+    printf("Enter a,b values");
+     
+ scanf("%d %d",&a,&b);
+ s=sum(a,b);
+ printf("%d",s);
+
+ }
+ int sum (char ,int y){ 
+    char x;
+    int y = 30;
+    x='A';
+    return x+y;
+ }
+
