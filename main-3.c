29c2e938f51457a31576aca787e80d17c3307666
HARSHA7317 <n.harshavardhan002@gmail.com>
2025-12-04 13:08:09 +0530

    Add files via upload

---
+++ main-3.c
@@ -0,0 +1,20 @@
+//asignment operators
+#include<stdio.h>
+int main()
+{
+   int a=5,c;
+   c=a;
+   printf("%d\n",c);
+   
+   c+=a;
+   printf("%d\n",c);
+   
+   c-=a;
+   printf("%d\n",c);
+   
+   c*=a;
+   printf("%d\n",c);
+   
+   return 0;
+}
+
